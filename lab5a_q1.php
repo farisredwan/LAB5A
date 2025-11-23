170f24a1b3a3a92f4852860926378d582cf44f6c
farisredwan <farisredwan728@gmail.com>
2025-11-23 13:32:17 +0800

    Create lab5a_q1.php

---
+++ lab5a_q1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q1</title>
+</head>
+<body>
+    <?php
+        $name = "Muhammad Faris bin Redwan";
+        $matricNumber = "CI240080";
+        $course = "Bachelor of Computer Science";
+        $yearOfStudy = "Year 2;";
+        $address = "No 24, Jalan Maju taman limbong maju";
+    ?>
+
+    <table>
+        <tr>
+            <td>Name</td>
+            <td><?php echo $name; ?></td>
+        </tr>
+        <tr>
+            <td>Matric Number</td>
+            <td><?php echo $matricNumber; ?></td>
+        </tr>
+        <tr>
+            <td>Course</td>
+            <td><?php echo $course; ?></td>
+        </tr>
+        <tr>
+            <td>Year of Study</td>
+            <td><?php echo $yearOfStudy; ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php echo $address; ?></td>
+        </tr>
+    </table>    
+</body>
+</html>
